8bb7d671d6f348f3cc5fd31b32a23b35c5f072d5
rafi <muhammad.farook@bs-23.net>
2020-04-24 22:21:31 +0600

    post method added and save in database

--- src/main/java/SpringMySql/controller/MainController.java
+++ src/main/java/SpringMySql/controller/MainController.java
@@ -1,10 +1,13 @@
 package SpringMySql.controller;
 
+import SpringMySql.model.User;
 import SpringMySql.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,6 +21,14 @@ public class MainController {
     public ModelAndView doHome() {
         ModelAndView mv = new ModelAndView("index");
         mv.addObject("list", userRepo.findAll());
+        return mv;
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public ModelAndView doSave(@RequestParam("name") String name, @RequestParam("age") int age, @RequestParam("gender") String gender) {
+        ModelAndView mv = new ModelAndView("redirect:/");
+        User user = new User(name,age,gender);
+       userRepo.save(user);
         return mv;
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,4 +1,4 @@
-<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <html>
 <head>
@@ -6,8 +6,21 @@
 </head>
 
 <body>
-<c:forEach var = "i" items="${list}">
-Item <c:out value = "${i.name}"/><p>
+<c:forEach var="i" items="${list}">
+Item <c:out value="${i.name}"/><p>
     </c:forEach>
+<form method="post" action="/save">
+    Name:<br>
+    <input type="text" name="name">
+    <br>
+    Age:<br>
+    <input type="number" name="age">
+    <br>
+    Gender:<br>
+    <input type="text" name="gender">
+    <br><br>
+    <input type="submit" value="submit">
+</form>
+
 </body>
 </html>
